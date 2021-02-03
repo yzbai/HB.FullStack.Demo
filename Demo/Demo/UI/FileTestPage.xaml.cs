2c4625bcd534bba79b897e0b91b39797aa268d29
yzbai <yzbai@brlite.com>
2021-02-03 21:19:16 +0800

    addd

---
+++ Demo/Demo/UI/FileTestPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HB.FullStack.Mobile.Base;
+using HB.FullStack.Mobile.Platforms;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo.UI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FileTestPage : BaseContentPage
+    {
+        private IFileHelper _fileHelper = DependencyService.Resolve<IFileHelper>();
+
+        public FileTestPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override IList<IBaseContentView?>? GetAllCustomerControls() => null;
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            TestImage.Source = ImageSource.FromStream(token => _fileHelper.GetResourceStreamAsync("bg_test", ResourceType.Drawable, null, token));
+        }
+    }
+}
