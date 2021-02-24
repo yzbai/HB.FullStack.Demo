bc980cd8e18d421256737b105fc0b1f9ed9e8141
yzbai <yzbai@brlite.com>
2021-02-25 07:28:56 +0800

    add

---
+++ Demo/Demo/UI/SkiaTouchTestPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HB.FullStack.XamarinForms.Base;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo.UI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SkiaTouchTestPage : BaseContentPage
+    {
+        public SkiaTouchTestPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override IList<IBaseContentView?>? GetAllCustomerControls()
+        {
+            return new IBaseContentView?[] { };
+        }
+    }
+}
