1142f322dde7d5032042f7334dc3f441917498d6
yzbai <yzbai@brlite.com>
2021-02-25 20:25:34 +0800

    add

---
+++ Demo/Demo/UI/XctTouchTestPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.CommunityToolkit.Extensions;
+
+namespace Demo.UI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class XctTouchTestPage : ContentPage
+    {
+        public ICommand BgTapCommand { get; set; }
+        public XctTouchTestPage()
+        {
+            InitializeComponent();
+
+            BgTapCommand = new Command(async () => {
+
+                await this.DisplayToastAsync("Background Clicked");
+
+            });
+
+            BindingContext = this;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Button", "Clicked", "Ok");
+        }
+    }
+}
