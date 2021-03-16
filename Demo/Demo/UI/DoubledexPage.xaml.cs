df6c7cf0d47437cddbd09f1cb44bb6d92023d943
yzbai <yzbai@brlite.com>
2021-03-16 20:42:18 +0800

    add

---
+++ Demo/Demo/UI/DoubledexPage.xaml.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HB.FullStack.Common;
+using HB.FullStack.XamarinForms;
+
+using SkiaSharp;
+using SkiaSharp.Extended;
+using SkiaSharp.Views.Forms;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo.UI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DoubledexPage : ContentPage
+    {
+
+        private const int DISPLAY_HOURS = 26;
+
+        private const float MAX_RADIUS_RATIO = 1f;
+
+        private const float INIT_RADIUS = 100f;
+
+        private float _initRadius;
+        private float _maxRadius;
+        private float _radiusGap;
+        private int _fullRoudCount;
+        private int _extraHours;
+        private bool _first = true;
+
+        public DoubledexPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CanvasView_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            SKImageInfo info = e.Info;
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+            using SKPaint paint2 = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = SKColors.Blue,
+                StrokeWidth = 20
+            };
+
+            if (_first)
+            {
+
+                _initRadius = INIT_RADIUS;
+                _maxRadius = Math.Min(info.Width, info.Height) / 2f * MAX_RADIUS_RATIO;
+
+                int displayHours = DISPLAY_HOURS;
+
+                //转几整圈
+                _fullRoudCount = 1 + (int)Math.Floor(displayHours / 12f);
+
+                //整圈后剩下的小时
+                _extraHours = displayHours % 12;
+
+                //每环之间等距
+                _radiusGap = (_maxRadius - _initRadius) / (_extraHours <= 2 ? _fullRoudCount - 1 : _fullRoudCount); //只是多出来两个多小时的话，不影响
+
+
+                _first = false;
+            }
+
+            canvas.Clear();
+            canvas.Translate(info.Width / 2f, info.Height / 2f);
+
+
+
+            List<(Time24Hour, Time24Hour)> blocks = new List<(Time24Hour, Time24Hour)>
+            {
+                (new Time24Hour(0,0), new Time24Hour(3,50)),
+                (new Time24Hour(4, 0), new Time24Hour(6, 15)),
+                (new Time24Hour(6, 30), new Time24Hour(6, 55)),
+                (new Time24Hour(6, 55), new Time24Hour(7, 0)),
+                (new Time24Hour(7, 0), new Time24Hour(7, 45)),
+                (new Time24Hour(8, 0), new Time24Hour(10, 55)),
+                (new Time24Hour(11, 30), new Time24Hour(12, 30)),
+                (new Time24Hour(13, 0), new Time24Hour(18, 0)),
+                (new Time24Hour(20, 0), new Time24Hour(20, 30)),
+                (new Time24Hour(21, 0), new Time24Hour(24, 0)),
+            };
+
+
+            foreach (var block in blocks)
+            {
+                paint2.Color = ColorUtil.RandomColor().Color.ToSKColor();
+                
+                var tbPaths = GetTimeBlockPath(block.Item1, block.Item2);
+
+                foreach (SKPath path in tbPaths)
+                {
+                    canvas.DrawPath(path, paint2);
+                    path.Dispose();
+                }
+            }
+
+            //Snail
+            DrawDial(canvas);
+
+            // Center
+            canvas.DrawPoint(0, 0, paint2);
+
+        }
+
+        private List<SKPath> GetTimeBlockPath(Time24Hour startTime, Time24Hour endTime)
+        {
+            float start = GetTimePercent(startTime);
+
+            float end = GetTimePercent(endTime);
+
+            //Same Ring
+            int startRing = (int)Math.Floor(start);
+            int endRing = (int)Math.Floor(end) == end ? (int)(end - 1) : (int)Math.Floor(end);
+
+            float innerRadius, outterRadius;
+
+            if (startRing == endRing)
+            {
+                innerRadius = GetRadiusByRingCount(startRing);
+                outterRadius = GetRadiusByRingCount(startRing + 1);
+
+                return new List<SKPath> { SKGeometry.CreateSectorPath(start, end, outterRadius, innerRadius) };
+            }
+
+            List<SKPath> paths = new List<SKPath>();
+
+            //先画完当前ring
+            innerRadius = GetRadiusByRingCount(startRing);
+            outterRadius = GetRadiusByRingCount(startRing + 1);
+
+            paths.Add(SKGeometry.CreateSectorPath(start, startRing + 1, outterRadius, innerRadius));
+
+            //画整圈
+
+            int curRing = startRing + 1;
+
+            while (curRing < endRing)
+            {
+                innerRadius = GetRadiusByRingCount(curRing);
+                outterRadius = GetRadiusByRingCount(curRing + 1);
+                paths.Add(SKGeometry.CreateSectorPath(curRing, curRing + 1, outterRadius, innerRadius));
+
+                curRing++;
+            }
+
+            //画剩下的
+            innerRadius = GetRadiusByRingCount(endRing);
+            outterRadius = GetRadiusByRingCount(endRing + 1);
+            paths.Add(SKGeometry.CreateSectorPath(endRing, end, outterRadius, innerRadius));
+
+            return paths;
+        }
+
+        /// <summary>
+        /// 第几圈, 从0 开始
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        private float GetRadiusByRingCount(int count)
+        {
+            return count * _radiusGap + _initRadius;
+        }
+
+        private static float GetTimePercent(Time24Hour time)
+        {
+            return (time.Day * 24 * 60 + time.Hour * 60 + time.Minute) / (12 * 60f);
+        }
+
+        private void DrawDial(SKCanvas canvas)
+        {
+            using SKPaint paint = new SKPaint { Style = SKPaintStyle.Stroke, Color = SKColors.Blue, StrokeWidth = 10 };
+
+            float curRadius = 0;
+
+            for (int i = 0; i < _fullRoudCount; i++)
+            {
+                curRadius = _initRadius + i * _radiusGap;
+
+                canvas.DrawCircle(0, 0, curRadius, paint);
+            }
+
+            //画剩余extraHours
+            curRadius += _radiusGap;
+
+            canvas.DrawArc(SKRect.Create(-curRadius, -curRadius, 2 * curRadius, 2 * curRadius), -90, _extraHours * 30, false, paint);
+        }
+    }
+}
--- Demo/Demo/UI/SnailPage.xaml.cs
+++ Demo/Demo/UI/SnailPage.xaml.cs
@@ -5,7 +5,11 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
+using HB.FullStack.Common;
+using HB.FullStack.XamarinForms;
+
 using SkiaSharp;
+using SkiaSharp.Extended;
 using SkiaSharp.Views.Forms;
 
 using Xamarin.Forms;
@@ -16,76 +20,376 @@ namespace Demo.UI
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SnailPage : ContentPage
     {
+        private const float MAX_RADIUS_RATIO = 1f; //占用屏幕最大比例
+
+        private const int DISPLAY_HOURS = 26;
+        private const int INIT_RADIUS = 50;
+
+        private const int NUMBER_MARK_RADIUS = 30;
+
+        //Consts
+        private const float RADIANS_ONE_DEGREE = (float)(Math.PI / 180);
+        private const int MINUTES_ONE_DAY = 24 * 60;
+        private const double RADIANS_ONE_DAY = 4 * Math.PI;
+        private const double RADIANS_ONE_HOUR = Math.PI / 6;
+        private const float RADIANS_ONE_MINUTE = (float)(Math.PI / 360);
+        private const double ONE_RADIAN_DEGREES = 180 / Math.PI;
+        private const double HALF_PI = 0.5 * Math.PI;
+        private const float TWO_PI = (float)(2 * Math.PI);
+
+        private const int DEGREES_ONE_HOUR = 30;
+        private const float MARK_LENGTH_RATIO = 0.2f;
+
+        //Draw Middle Data
+        private float _maxRadius;
+        private float _initRadius;
+        private float _radiusGap;
+        private int _roudCount;
+        private int _totalDegrees;
+        private float _totalRadian;
+        private float _oneDegreeRadiusIncr;
+        private float _oneRadianRadiusIncr;
+
+        //小刻度的长度
+        private float _markLength;
+
         public SnailPage()
         {
             InitializeComponent();
         }
 
+        private bool _first = true;
+
+
         private void CanvasView_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
-{
+        {
             SKImageInfo info = e.Info;
             SKSurface surface = e.Surface;
             SKCanvas canvas = surface.Canvas;
 
+            using SKPaint paint2 = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = SKColors.Blue,
+                StrokeWidth = 20
+            };
+
             canvas.Clear();
 
             canvas.Translate(info.Width / 2f, info.Height / 2f);
-            //canvas.RotateDegrees(-90);
 
-            float maxRadius = Math.Min(info.Width, info.Height) / 2f;
-
-            float initRadius = 50;
-
-            float gapRadius = maxRadius - initRadius;
-
-            SKPoint initPoint = new SKPoint(0,0);
-
-            using (SKPath path = new SKPath())
+            if (_first)
             {
-                int count = 0;
-                for (float angle = 0; angle < 360 * 3; angle += 1, count++)
-                {
-                    float curRaius = initRadius + gapRadius  / (36*30)  * count;
-                    double radians = Math.PI * angle / 180;
-                    float x = curRaius * (float)Math.Cos(radians);
-                    float y = curRaius * (float)Math.Sin(radians);
-                    
-                    SKPoint point = new SKPoint(x, y);
+                _maxRadius = Math.Min(info.Width, info.Height) / 2f * MAX_RADIUS_RATIO;
 
-                    if (angle == 0)
-                    {
-                        initPoint = point;
-                        path.MoveTo(point);
-                    }
-                    else
-                    {
-                        path.LineTo(point);
-                    }
+                _initRadius = INIT_RADIUS;
+
+                int displayHours = DISPLAY_HOURS;
+
+                //转几圈
+                _roudCount = 1 + (int)Math.Floor(displayHours / 12f);
+
+                int extraHoursDegrees = (displayHours % 12) * DEGREES_ONE_HOUR;
+
+                //总共多少度
+                _totalDegrees = _roudCount * 360 + extraHoursDegrees;
+
+                //总共多少弧度
+                _totalRadian = _totalDegrees * RADIANS_ONE_DEGREE;
+
+                //每一度，增长的半径长度, 
+                _oneDegreeRadiusIncr =
+                    extraHoursDegrees > 75 //只是多出来两个多小时的话，不影响
+                    ? (_maxRadius - _initRadius) / _totalDegrees
+                    : (_maxRadius - _initRadius) / (_totalDegrees - extraHoursDegrees);
+
+                //每一弧度，增长的半径长度
+                _oneRadianRadiusIncr = 
+                    extraHoursDegrees > 75 //只是多出来两个多小时的话，不影响
+                    ? (_maxRadius - _initRadius) / _totalRadian 
+                    :(_maxRadius - _initRadius) / (_totalRadian - extraHoursDegrees * RADIANS_ONE_DEGREE);
+
+                //两根螺旋线间的距离
+                _radiusGap = (GetRadiusByRadian(_roudCount * TWO_PI) - _initRadius) / _roudCount;
+
+                _markLength = _radiusGap * MARK_LENGTH_RATIO;
+
+                _first = false;
+            }
+
+            //Draw TimeBlock
+
+            List<(Time24Hour, Time24Hour)> blocks = new List<(Time24Hour, Time24Hour)>
+            {
+                (new Time24Hour(0,0), new Time24Hour(3,50)),
+                (new Time24Hour(4, 0), new Time24Hour(6, 15)),
+                (new Time24Hour(6, 30), new Time24Hour(6, 55)),
+                (new Time24Hour(6, 55), new Time24Hour(7, 0)),
+                (new Time24Hour(7, 0), new Time24Hour(7, 45)),
+                (new Time24Hour(8, 0), new Time24Hour(11, 55)),
+                (new Time24Hour(12, 0), new Time24Hour(13, 0)),
+                (new Time24Hour(13, 0), new Time24Hour(18, 0)),
+                (new Time24Hour(20, 0), new Time24Hour(20, 30)),
+                (new Time24Hour(21, 0), new Time24Hour(24, 0)),
+            };
+
+
+            foreach (var block in blocks)
+            {
+                SKPath tbPath = GetTimeBlockPath(block.Item1, block.Item2);
+
+                paint2.Color = ColorUtil.RandomColor().Color.ToSKColor();
+
+                canvas.DrawPath(tbPath, paint2);
+            }
+
+            //Snail
+
+            DrawSnail(canvas);
+
+            // Center
+
+
+            canvas.DrawPoint(0, 0, paint2);
+
+        }
+
+        /// <summary>
+        /// 表盘
+        /// </summary>
+        /// <param name="canvas"></param>
+        /// <param name="initRadius"></param>
+        /// <param name="diffRadius"></param>
+        private void DrawSnail(SKCanvas canvas)
+        {
+
+
+            using SKPaint spiralPaint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Black,
+                StrokeWidth = 4,
+                IsAntialias = true
+            };
+
+            using SKPaint hourPaint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Black,
+                StrokeWidth = 4,
+                IsAntialias = true
+            };
+
+            using SKPaint littleCirclePatin = new SKPaint
+            {
+
+                Style = SKPaintStyle.StrokeAndFill,
+                Color = SKColors.Black,
+                StrokeWidth = 1,
+                IsAntialias = true
+            };
+
+            using SKPaint markPaint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Black,
+                StrokeWidth = 4,
+                IsAntialias = true,
+                StrokeCap = SKStrokeCap.Round,
+                PathEffect = SKPathEffect.CreateDash(new float[] { 30, 30 }, 0)
+            };
+
+            using SKPaint numberPaint = new SKPaint
+            {
+                Color = SKColors.White,
+            };
+
+            float originalTextWidth = numberPaint.MeasureText("8");
+            float textWithSizeRatio = originalTextWidth / numberPaint.TextSize;
+            numberPaint.TextSize = NUMBER_MARK_RADIUS * 2 / MathF.Sqrt(2);
+            float oneDigitalWidth = numberPaint.TextSize * textWithSizeRatio;
+
+            //TODO: 可以只在有变动时生成
+            using SKPath spiralPath = new SKPath();
+            using SKPath hourPath = new SKPath();
+            using SKPath markPath = new SKPath();
+
+            int hourNumber = 0;
+
+            for (float degree = 0; degree <= _totalDegrees; degree += 1)
+            {
+                float curRaius = GetRadiusByDegree(degree);
+                double radians = degree * RADIANS_ONE_DEGREE - HALF_PI;
+
+                float cos = (float)Math.Cos(radians);
+                float sin = (float)Math.Sin(radians);
+
+                float x = curRaius * cos;
+                float y = curRaius * sin;
+
+                float outterX = (curRaius + _radiusGap) * cos;
+                float outterY = (curRaius + _radiusGap) * sin;
+
+                if (degree == 0)
+                {
+                    spiralPath.MoveTo(x, y);
+                }
+                else
+                {
+                    spiralPath.LineTo(x, y);
                 }
 
-                path.LineTo(initPoint);
-
-                SKPaint paint = new SKPaint
+                if (hourNumber <= DISPLAY_HOURS)
                 {
-                    Style = SKPaintStyle.Stroke,
-                    Color = SKColors.Red,
-                    StrokeWidth = 5
-                };
+                    if (degree % 30 == 0)
+                    {
+                        //画小时线
+                        canvas.DrawLine(x, y, outterX, outterY, spiralPaint);
 
-                canvas.DrawPath(path, paint);
+                        hourNumber++;
 
-                SKPaint paint2 = new SKPaint
+                    }
+                    else if (degree * 10 % 75 == 0)
+                    {
+                        //画15分钟线
+                        //canvas.DrawLine(x, y, outterX, outterY, markPaint);
+                    }
+                }
+            }
+
+            canvas.DrawPath(spiralPath, spiralPaint);
+
+            hourNumber = 0;
+
+            for (float degree = 0; degree <= _totalDegrees; degree += 30)
+            {
+                float curRaius = GetRadiusByDegree(degree);
+                double radians = degree * RADIANS_ONE_DEGREE - HALF_PI;
+
+                float cos = (float)Math.Cos(radians);
+                float sin = (float)Math.Sin(radians);
+
+                float x = curRaius * cos;
+                float y = curRaius * sin;
+
+                float outterX = (curRaius + _radiusGap) * cos;
+                float outterY = (curRaius + _radiusGap) * sin;
+
+
+
+                if (hourNumber <= DISPLAY_HOURS)
                 {
-                    Style = SKPaintStyle.Fill,
-                    Color = SKColors.Blue,
-                    StrokeWidth = 20
-                };
+                    if (degree % 30 == 0)
+                    {
 
-                canvas.DrawPoint(0, 0, paint2);
+                        canvas.DrawCircle(outterX, outterY, NUMBER_MARK_RADIUS, littleCirclePatin);
+
+                        string text = (hourNumber % 24).ToString();
+
+                        float textWidth = text.Length * oneDigitalWidth;
+
+                        canvas.DrawText(hourNumber.ToString(), outterX - textWidth / 2f, outterY + numberPaint.TextSize / 2f, numberPaint);
+
+                        hourNumber++;
+                    }
+                }
             }
 
 
 
+            //return path;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="maxRadius">最大半径</param>
+        /// <param name="initRadius">初始半径</param>
+        /// <param name="radiusGap">螺旋环的间距</param>
+        /// <param name="startTime">开始时间</param>
+        /// <param name="endTime">结束时间</param>
+        /// <returns></returns>
+        private SKPath GetTimeBlockPath(Time24Hour startTime, Time24Hour endTime)
+        {
+            SKPath tbPath = new SKPath();
+
+            float startRadian = GetTimeRadian(startTime);
+            float endRadian = GetTimeRadian(endTime);
+
+            float startRadius = GetRadiusByRadian(startRadian);
+            float startOutterRadius = startRadius + _radiusGap;
+
+            float endRadius = GetRadiusByRadian(endRadian);
+            float endOutterRadius = endRadius + _radiusGap;
+
+            double degrees = (endRadian - startRadian) * ONE_RADIAN_DEGREES;  //180 / Math.PI;
+
+            double oneDegreeRadiusIncr = (endRadius - startRadius) / degrees;
+
+            for (int degree = 0; degree <= degrees; degree++)
+            {
+                double curRadian = startRadian + degree * RADIANS_ONE_DEGREE;
+                double curRadius = startOutterRadius + degree * oneDegreeRadiusIncr;
+
+                float curX = (float)(curRadius * Math.Cos(curRadian - HALF_PI));
+                float curY = (float)(curRadius * Math.Sin(curRadian - HALF_PI));
+
+                if (degree == 0)
+                {
+                    tbPath.MoveTo(curX, curY);
+                }
+                else
+                {
+                    tbPath.LineTo(curX, curY);
+                }
+            }
+
+            tbPath.LineTo((float)(endOutterRadius * Math.Cos(endRadian - HALF_PI)), (float)(endOutterRadius * Math.Sin(endRadian - HALF_PI)));
+
+            for (int degree = 0; degree <= degrees; degree++)
+            {
+                double curRadian = endRadian - degree * RADIANS_ONE_DEGREE;
+                double curRadius = endRadius - degree * oneDegreeRadiusIncr;
+
+                float curX = (float)(curRadius * Math.Cos(curRadian - HALF_PI));
+                float curY = (float)(curRadius * Math.Sin(curRadian - HALF_PI));
+
+                tbPath.LineTo(curX, curY);
+            }
+
+            tbPath.LineTo((float)(startRadius * Math.Cos(startRadian - HALF_PI)), (float)(startRadius * Math.Sin(startRadian - HALF_PI)));
+
+            tbPath.Close();
+
+            return tbPath;
+        }
+
+        /// <summary>
+        /// 时间弧度，从0点开始
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static float GetTimeRadian(Time24Hour time)
+        {
+            //TODO: 提前把数字都计算出来
+            return (time.Day * 24 * 60 + time.Hour * 60 + time.Minute) * RADIANS_ONE_MINUTE;
+        }
+
+        /// <summary>
+        /// 根据弧度，算出半径
+        /// </summary>
+        /// <param name="radian"></param>
+        /// <param name="initRadius"></param>
+        /// <param name="maxRadius"></param>
+        /// <returns></returns>
+        public float GetRadiusByRadian(float radian)
+        {
+            return _initRadius + radian * _oneRadianRadiusIncr;
+        }
+
+        public float GetRadiusByDegree(float degree)
+        {
+            return _initRadius + degree * _oneDegreeRadiusIncr;
         }
     }
 }
